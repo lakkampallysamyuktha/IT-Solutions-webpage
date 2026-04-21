80c0d7aa76af590527e768e1c645e3a6cffe51b8
lakkampallysamyuktha <87126592+lakkampallysamyuktha@users.noreply.github.com>
2026-04-21 19:45:44 +0530

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -2,10 +2,6 @@ function scrollToSection(id){
   document.getElementById(id).scrollIntoView({behavior:"smooth"});
 }
 
-function goToRegister(){
-  window.location.href = "register.html";
-}
-
 function toggleMenu(){
   document.querySelector(".nav-links").classList.toggle("active");
 }
